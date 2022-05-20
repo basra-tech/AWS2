9cb545964b6da0f1d0cd15335f4a647fbcd7ff7e
EC2 Default User <ec2-user@ip-172-31-3-166.eu-west-1.compute.internal>
2022-05-20 13:23:47 +0000

    Configure API invokeURL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
-// Add your API invoke URL below, configured in Module 3. 
-
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://f7ztssksma.execute-api.eu-west-1.amazonaws.com/prod'
     }
 }
+
